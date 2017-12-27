fa3553ce0e05faf224ef3fe7ceb8ddc046411276
Allis Gao <gaojz017@163.com>
2017-12-27 18:51:11 +0800

    Exercises in Chapter2 has been completely.

---
+++ Section1_Getting_started/Chapter2/2.3.1_name.py
@@ -0,0 +1,10 @@
+#! python3
+name = "ada lovelace"
+print("name       :" + name)
+print("name1 title: " + name.title())
+print("name UPPER :" + name.upper())
+print("name lower :" + name.lower())
+
+name2 = "ADA LOVElace"
+print("name2 title: " + name2.title())
+print("name2 str  : " + name2)
---
+++ Section1_Getting_started/Chapter2/2.3.5_apostrophe.py
@@ -0,0 +1,5 @@
+# python3
+
+message = "One of Python's strengths is its diverse community"
+print(message)
+
