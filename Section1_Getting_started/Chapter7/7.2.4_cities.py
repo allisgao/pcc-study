caebb2d0c1f2726d50716c41b73762aeebe136b9
Allis.Gao <gaojz017@163.com>
2018-01-18 16:07:28 +0800

    some practice of Chapter7 in 2018.118

---
+++ Section1_Getting_started/Chapter7/7.2.4_cities.py
@@ -0,0 +1,10 @@
+#! python3
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.) \n"
+
+while True:
+    city = input(prompt)
+    if city.lower() == 'quit':
+        break
+    else:
+        print("I'd love to go to %s!" % (city.lower().title()))
