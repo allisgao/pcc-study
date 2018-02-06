f8dbb4ae872482a3f173f640a28b90f3a5688d49
Allis Gao <gaojz017@163.com>
2018-02-06 16:02:00 +0800

    18.1 finished.

--- Section2_Projects/Program3/learning_log/learning_log/settings.py
+++ Section2_Projects/Program3/learning_log/learning_log/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my applications
+    'learning_logs',
 ]
 
 MIDDLEWARE = [
