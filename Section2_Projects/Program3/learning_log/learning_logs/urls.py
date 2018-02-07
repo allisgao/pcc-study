139a67452dd0d0fc142a587c17d7c4037cbd8a39
Allis Gao <gaojz017@163.com>
2018-02-07 11:48:43 +0800

    add ','

--- Section2_Projects/Program3/learning_log/learning_logs/urls.py
+++ Section2_Projects/Program3/learning_log/learning_logs/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     # homepage
     # url(r'^$', views.index, name='index')
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
 ]
