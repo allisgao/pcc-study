c574c36f0b25d7a0bb82e0bb2096f90cbee8be02
Allis Gao <gaojz017@163.com>
2018-02-07 15:44:20 +0800

    pizza under 18.3 finished

---
+++ Section2_Projects/Program3/pizza/pizzas/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #homepage
+    path('', views.index, name='index'),
+]
--- Section2_Projects/Program3/pizza/pizzas/views.py
+++ Section2_Projects/Program3/pizza/pizzas/views.py
@@ -1,3 +1,10 @@
+""" define pizzas' URL pattern"""
+
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """ pizzas' index page"""
+    return render(request, 'pizzas/index.html')
+
+
