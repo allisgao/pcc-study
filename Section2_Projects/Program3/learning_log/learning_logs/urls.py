e91e76ca4ddfb7418b528d3cec54ccbaad4a40d5
Allis Gao <gaojz017@163.com>
2018-02-08 23:24:03 +0800

    learning_logs in 18.4 finfished

--- Section2_Projects/Program3/learning_log/learning_logs/urls.py
+++ Section2_Projects/Program3/learning_log/learning_logs/urls.py
@@ -3,8 +3,16 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
+app_name = 'learning_logs'
+
 urlpatterns = [
     # homepage
     # url(r'^$', views.index, name='index')
     path('', views.index, name='index'),
+
+    # show all topics
+    path('topics/', views.topics, name='topics'),
+
+    # details of special topics
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
