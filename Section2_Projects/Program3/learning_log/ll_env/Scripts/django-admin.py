de3c8674ed0d143802cd2c27290e851f29821482
Allis Gao <gaojz017@163.com>
2018-02-08 23:21:49 +0800

    clear old version files

--- Section2_Projects/Program3/learning_log/ll_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!e:\mycodes\python\pcc\section2_projects\program3\learning_log\ll_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
