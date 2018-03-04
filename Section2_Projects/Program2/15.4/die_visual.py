ebbdc86c5f4df29b7eebce5694bde0f9392216a2
gaojzh <gaojz017@sina.com>
2018-03-04 10:55:57 +0800

    export the result in hist

--- Section2_Projects/Program2/15.4/die_visual.py
+++ Section2_Projects/Program2/15.4/die_visual.py
@@ -1,5 +1,5 @@
 from die import Die
-
+import pygal
 
 # create a D6
 die = Die()
@@ -16,4 +16,14 @@ for value in range(1, die.number_sides + 1):
     frequencey = results.count(value)
     frequencies.append(frequencey)
 
-print(frequencies)
+
+# visualize the result
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
