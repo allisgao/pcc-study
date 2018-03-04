647b96338b1f247d694d6778364ef469d499a83b
gaojzh <gaojz017@sina.com>
2018-03-04 10:37:59 +0800

    statistic the frequency

--- Section2_Projects/Program2/15.4/die_visual.py
+++ Section2_Projects/Program2/15.4/die_visual.py
@@ -6,8 +6,14 @@ die = Die()
 
 # roll die some times, and store the result in a list
 results = []
-for roll_number in range(100):
+for roll_number in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# analize the resultes
+frequencies = []
+for value in range(1, die.number_sides + 1):
+    frequencey = results.count(value)
+    frequencies.append(frequencey)
+
+print(frequencies)
